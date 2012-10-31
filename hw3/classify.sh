a618c5d3b07ce7b97cc4717a434cdccd8177a7ce
Pankaj More <pankajmore@gmail.com>
2012-10-31 18:36:23 +0530

    classifcatoion output spec

--- hw3/classify.sh
+++ hw3/classify.sh
@@ -13,13 +13,16 @@ convert_csv_to_arff()
 
 train_model()
 {
-    java weka.classifiers.bayes.NaiveBayes -K -t classifier_unordered_train.arff -d naive.model > /dev/null
+    java weka.classifiers.bayes.NaiveBayes -K -t classifier_unordered_train.arff -d naive.model  > /dev/null
 }
 
 test_model()
 {
-    java weka.classifiers.bayes.NaiveBayes -v -o -l naive.model -T test_data.arff > results.txt 
-    echo "Results of classification can be seen at results.txt"
+    java weka.classifiers.bayes.NaiveBayes -v -o -l naive.model -T test_data.arff -classifications "weka.classifiers.evaluation.output.prediction.PlainText" > results.txt 
+}
+get_statistics()
+{
+    java weka.classifiers.bayes.NaiveBayes -v -o -l naive.model -T test_data.arff > stats.txt 
 }
 # If CLASSPATH variable is not set , we export it 
 if [ -z $CLASSPATH ]; then
@@ -30,6 +33,9 @@ if [ $1 ]; then
     convert_csv_to_arff $1
     train_model
     test_model $1
+    get_statistics
+    awk 'NR >= 6 { print $3 }' results.txt | cut -c 3 > output.txt
+    echo "See output.txt for class labels"
 else
     echo "Usage: "
     echo "./classify.sh <testfile>"
